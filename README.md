4086d0e4d99491c46b61a364fe3b1903783a5c8d
Philip Lassen <philiplassen@gmail.com>
2019-04-30 14:19:16 +0200

    Finished the Preliminary gamelog webscrape, Updated README, and added .gitignore

--- README.md
+++ README.md
@@ -3,18 +3,41 @@
 
 This project aims to make accurate predictions of player performances and use these predictions to select lineups on the popular fantasy sport sights lineups
 
-
   - Data Retrieval
   - Statistical Modelling
   - Betting Strategey
 
 ## Software 
 
-This project is primarilty devoloped with Python 3.
+This project is primarilty devoloped with Python 3, and uses a few popular Python libraries.
+
+### Prerequisites
+
+The project uses the libraries: requests, bs4, and pandas. They can be installed with pip
+
+```
+pip3 install pandas bs4 requests
+```
+
+### Installing
+
+The project can be installed by simply cloning the repository
+```
+git clone https://github.com/philiplassen/StatFloor.git
+```
+
+
+### Running
+
+We can run the data Webscrape with the following command as an example.
+```
+python3 player_scrape.py "mike trout" 2018
+``` 
+This stores Mike Trout's gamelogs from 2018 in a csv
+
 
 ### Team Members
 - Philip Lassen
 - Sid Mukkamala
 - David Majercak
-
 
--- src/player_scrape.py
+++ src/player_scrape.py
@@ -1,80 +1,96 @@
 #!/usr/bin/env python3
 
 """
-Usage 
-
-python3 player_scrape PID
-
-Output 
-
-PID.csv
-
+Library for Scraping Game Logs for MLB
+players from baseball-reference
 """
 
 import sys
-import os
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup as bs
 
-def log(message):
-  if '-v' in sys.argv:
-    print(message)
-
-pid = sys.argv[1]
-pid = "troutmi01"
-
-
-
-response = requests.get("https://www.baseball-reference.com/players/gl.fcgi?id=" + pid + "&t=b&year=2018")
-
-
-if not response:
-  print("Coud not find Webpage")
-  sys.exit() # Exit the Program
-
-# ------- Proccessing Web Page ---------
-log("Web Page Found")
-log(response.text)
-soup = bs(response.content, 'html.parser')
-game_log_table = soup.findAll('table')[-1]
-game_log_rows = game_log_table.findAll('tr')
-count = 0
-
-"""
-def getContent(contents):
-  if contents == []:
-    return None
-  else if contents:
-"""
-
+YEAR = "2017"
+PLAYER = "mike trout"
+if len(sys.argv) > 1:
+  PLAYER = sys.argv[1]
+if len(sys.argv) > 2: 
+  YEAR = sys.argv[2]
+print(PLAYER)
+print(YEAR)
+  
 def getContent(array):
+  """
+  Formats the content of an Array based 
+  on the result of the array 
+  comprehension - [t.contents for t in tds]
+  """
   result = []
   for a in array:
     if len(a) == 0:
       result += [None]
     elif type(a[0]) == str:
-      #print("yo this worked")
       result += a 
     else:
-      #print(a[0])
       result += [a[0].string]
   return result
 
-data = []
-for tr in game_log_rows:
-  tds = tr.findAll('td')
-  results = [t.contents for t in tds] 
-  res = getContent(results)
-  
-  if len(res) > 0:
-    data += [res] 
-
-
+# Column Names for the DataFrames and CSV
 
 headers = ["Rk",	"Gcar",	"Gtm",	"Date",	"Tm",		"Opp",	"Rslt",	"Inngs",	
 "PA",	"AB",	"R",	"H",	"2B",	"3B",	"HR",	"RBI",	"BB",	"IBB",	
 "SO",	"HBP",	"SH",	"SF",	"ROE",	"GDP",	"SB",	"CS",	"BA",	"OBP",	
 "SLG",	"OPS",	"BOP",	"aLI",	"WPA",	"RE24",	"DK",	"FD", "Pos"]
-df = pd.DataFrame(data, columns = headers)
-print(df)
+
+def generatePID(val):
+  """
+  Generate pid based on player
+  IMPORTANT
+  DOES NOT WORK FOR NON UNIQUE NAME
+  """
+  index = val.index(' ')
+  return val[index + 1:index + 6] + val[0:2] + "01"
+ 
+def getUrl(pid, year):
+ """
+ Generate URL (as a string) to be 
+ scraped for gamelogs for a given pid and year
+ """
+ print(year)
+ return "https://www.baseball-reference.com/players/gl.fcgi?id=" + pid + "&t=b&year=" + year
+
+
+def getData(player_name, year = "2019", verbose = False):
+  """Generates a CSV of the GameLogs of a given player
+  for the specfified year. The CSV has 37 columns and
+  the number of rows dpeend on the number of games that
+  the player played in that year"""
+  pid = generatePID(player_name)
+  url = getUrl(pid, year)
+  print(url)
+  response = requests.get(url)
+  if not response:
+    print("Coud not find Webpage")
+    return #Exit the program 
+  soup = bs(response.content, 'html.parser')
+  game_log_table = soup.findAll('table')[-1]
+  game_log_rows = game_log_table.findAll('tr')
+  data = []
+  for tr in game_log_rows:
+    tds = tr.findAll('td')
+    results = [t.contents for t in tds] 
+    res = getContent(results)
+    if len(res) > 0:
+      data += [res]
+  df = pd.DataFrame(data, columns = headers)
+  df.to_csv(pid + "-" + year + ".csv")
+
+
+""" 
+If Command Line Arguements were given run
+Code snippit Below
+"""
+
+if len(sys.argv) > 1:
+  getData(PLAYER, year = YEAR)
+

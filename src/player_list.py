d90018adceb7d7e6b7a8e4b49ea60e8b6e79ae70
Philip Lassen <philiplassen@gmail.com>
2019-05-04 16:29:01 +0200

    Added Data directory with scraper results

--- src/player_list.py
+++ src/player_list.py
@@ -2,6 +2,7 @@
 
 """
 Generate a list of Active Player Names and the respective Player IDs
+and store them in a CSV file
 """
 
 
@@ -11,6 +12,10 @@ import pandas as pd
 from bs4 import BeautifulSoup as bs
 
 def getAllLetterPlayers(l):
+  """
+  Get all Player names starting with l and corresponding
+  Reference ID's from Baseball reference as an array
+  """
   url = "https://www.baseball-reference.com/players/" + l + "/"
   response = requests.get(url)
   if not response:
@@ -18,21 +23,24 @@ def getAllLetterPlayers(l):
     return #Exit function
   soup = bs(response.content, 'html.parser')
   players = soup.findAll('strong')
-  print(players)
   return [(p1.find('a').text, p1.find('a').attrs['href'][11:-6]) for p1 in players 
                                                           if p1.find('a') != None]
     
 
 def getAllPlayers():
-  alphabet = "bcdefghijklmnopqrstuvwxyz"
+  """
+  Generate A CSV of all active players from Baseball Reference
+  """
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
   players = []
   for letter in alphabet:
-    print(letter)
     ps = getAllLetterPlayers(letter)
-    print(ps)
     players += ps
-  print(players)
+  df = pd.DataFrame(players, columns = ["Name", "PID"])
+  df.to_csv("data/pids.csv")
   return players
+
+
 
 getAllPlayers()
     
